972a9d97048654b2883f28ed65eb50d9c6c990a4
richmondazadze <richmondazadze1313@gmail.com>
2024-08-09 23:58:45 +0000

    Fixed Env

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYPZusFFdjOOd9XpWt51v6xwi3mUQ9Ru0",
-  authDomain: "pantry-management-55082.firebaseapp.com",
-  projectId: "pantry-management-55082",
-  storageBucket: "pantry-management-55082.appspot.com",
-  messagingSenderId: "784345798671",
-  appId: "1:784345798671:web:de0b6ebabff9f713d8ab7a",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
